5a7505556eaf373c233869a4b26c2009d5122921
Michał Zając <michal.zajac@gmail.com>
2015-07-28 21:26:50 +0200

    Move to Rails Assets + translations

--- Gemfile
+++ Gemfile
@@ -1,15 +1,20 @@
 source 'https://rubygems.org'
-gem 'sinatra'
+gem 'sinatra', require: 'sinatra/base'
 gem 'sinatra-partial', require: 'sinatra/partial'
+gem 'sinatra-asset-pipeline', require: 'sinatra/asset_pipeline'
 gem 'sinatra-r18n', require: 'sinatra/r18n'
-gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
-gem 'yui-compressor', :require => 'yui/compressor'
+gem 'uglifier'
 gem 'haml'
 gem 'pony'
 
-
 group :development do
+  gem 'pry-byebug'
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'thin'
+end
+
+source 'https://rails-assets.org' do
+  gem 'rails-assets-jquery'
+  gem 'rails-assets-bootstrap'
 end
--- sesja.rb
+++ sesja.rb
@@ -1,22 +1,36 @@
 class SesjaLinuksowa < Sinatra::Application
 
   configure do
+    enable :sessions
+
     # Nie zapomnij zmienić tego!
-    set :edition => ""
+    set :edition => "13"
 
     register Sinatra::R18n
-    R18n::I18n.default = 'pl'
-    register Sinatra::AssetPack
+
+    set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+    set :assets_css_compressor, :sass
+    set :assets_js_compressor, :uglifier
+    register Sinatra::AssetPipeline
+
+    if defined?(RailsAssets)
+      RailsAssets.load_paths.each do |path|
+        settings.sprockets.append_path(path)
+      end
+    end
+
     register Sinatra::Partial
     set :partial_template_engine, :haml
+
     set :haml, :format => :html5
+
     set :default_to => "sesja@linuksowa.pl"
     set :email_options, {
       :from => "asiwww@tramwaj.asi.pwr.wroc.pl"
     }
 
   end
-  
+
   if settings.edition.empty?
     abort("Edycja Sesji nie jest ustawiona, zajrzyj do pliku sesja.rb!")
   end
@@ -25,35 +39,16 @@ class SesjaLinuksowa < Sinatra::Application
     use BetterErrors::Middleware
     BetterErrors.application_root = File.expand_path('..', __FILE__)
   end
-
-  assets do
-    serve '/css', :from => "assets/css"
-
-    serve '/js', :from => "assets/js"
-
-    serve '/jquery', :from => "assets/jquery"
-    js :jquery, '/jquery/jquery.js', [
-      '/jquery/dist/jquery.js'
-    ]
-
-    serve '/bootstrap', :from => "assets/bootstrap"
-    css :bootstrap, '/bootstrap/bootstrap.css', [
-      '/bootstrap/dist/css/bootstrap.css',
-      '/bootstrap/dist/css/bootstrap-theme.css'
-    ]
-    js :bootstrap, '/bootstrap/bootstrap.js', [
-      '/bootstrap/dist/js/bootstrap.js'
-    ]
-
-    js_compression :yui
-    css_compression :yui
+  
+  get '/' do
+    redirect "/pl"
   end
 
-  get '/' do
+  get '/:locale/?' do
     haml :index, :locals => {:edition => settings.edition}
   end
   
-  post '/' do
+  post '/:locale/?' do
 
     # Prosty filtr antyspamowy
     redirect '/' unless params[:email].empty?
